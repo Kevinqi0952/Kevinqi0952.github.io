8b9683e6e6c93b34a5ca0bc83b705705da0feffe
wujiaqi <372219097@qq.com>
2016-03-31 15:13:26 +0800

    add Gmlm

---
+++ work_test/Gmlm/js/Gmlm_Signin.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wujiaqi 2016/3/31.
+ */
+//SingIn start...
+$(function(){
+    var signinHeight = $(document).height();
+    var signinNewHeight = signinHeight/20 + "rem";
+    console.log(signinHeight);
+    $('.signin-main').css('height',signinNewHeight);
+})
+//SingIn end...
