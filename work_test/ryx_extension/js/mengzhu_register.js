4490f5a116b2a4937320d7203110fb7ebe1d541d
wujiaqi <372219097@qq.com>
2016-04-06 18:05:20 +0800

    change

--- work_test/ryx_extension/js/mengzhu_register.js
+++ work_test/ryx_extension/js/mengzhu_register.js
@@ -1,3 +1,28 @@
 /**
  * Created by yurong on 2016/4/6.
  */
+    //获取验证码倒计时
+$(function  () {
+    var validCode=true;
+    $("#code-btn-active").click (function() {
+        var time= 60;
+        var code=$(this);
+
+        code.addClass("code-btn-active");
+        if (validCode) {
+            validCode=false;
+            var t = setInterval(function() {
+                time--;
+                code.html(time+ "S");
+                code.addClass("code-btn-active");
+                if (time==0) {
+                    clearInterval(t);
+                    code.html("获取验证码");
+                    validCode=true;
+                    code.addClass("code-btn-active");
+                    code.removeClass("code-btn-active");
+                }
+            },1000)
+        }
+    })
+})
