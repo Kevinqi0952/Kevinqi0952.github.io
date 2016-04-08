af9e5d62036ee9e70cb322fe5f2cb03a7dd68dc0
wujiaqi <372219097@qq.com>
2016-04-08 16:09:44 +0800

    update info

--- work_test/ryx_extension/js/mengzhu_gift.js
+++ work_test/ryx_extension/js/mengzhu_gift.js
@@ -1,3 +1,18 @@
 /**
  * Created by wujiaqi on 2016/4/8.
  */
+
+$(function(){
+    $('.chose-gift').click(function(){
+        var checked = $(this).children('.chose-checked');
+
+        //商品选择
+        if(checked.hasClass('chose-checked-active')){
+
+        } else{
+            $('.chose-gift').children('.chose-checked').removeClass('chose-checked-active');
+            $(checked).addClass('chose-checked-active');
+        }
+    })
+    //切换商品及详情动画
+})
