c1ceb5b8b52bb1b11d91b95b409843313f0ad210
wujiaqi <372219097@qq.com>
2016-04-12 17:39:11 +0800

    update info

--- work_test/ryx_extension/js/mengzhu_gift.js
+++ work_test/ryx_extension/js/mengzhu_gift.js
@@ -5,14 +5,31 @@
 $(function(){
     $('.chose-gift').click(function(){
         var checked = $(this).children('.chose-checked');
-
-        //商品选择
-        if(checked.hasClass('chose-checked-active')){
-
-        } else{
+        var activeDiv = $(this).index();
+        //选择样式
+        if($('.chose-gift').hasClass('chose-chose')){
             $('.chose-gift').children('.chose-checked').removeClass('chose-checked-active');
             $(checked).addClass('chose-checked-active');
         }
+        //判断选择页面
+        if(activeDiv == 0){
+            $('.chose-gift').eq(0).removeClass('chose-not-chose');
+            $('.gift-giftB').hide()
+            $('.chose-gift').eq(0).addClass('chose-chose');
+            $('.chose-gift').eq(1).addClass('chose-not-chose');
+            $('.gift-giftA').show()
+            $('#gift-next').click(function(){
+                location.href = 'mengzhu_giftA.html';
+            })
+        } else{
+            $('.gift-giftA').hide()
+            $('.chose-gift').eq(1).removeClass('chose-not-chose');
+            $('.chose-gift').eq(1).addClass('chose-chose');
+            $('.chose-gift').eq(0).addClass('chose-not-chose');
+            $('.gift-giftB').show()
+            $('#gift-next').click(function(){
+                location.href = 'mengzhu_giftB.html';
+            })
+        }
     })
-    //切换商品及详情动画
 })
