980c982c9ef15a68b8cc3f555594255b80662c90
BigMouth <372219097@qq.com>
2016-04-25 15:29:05 +0800

    cahnge

---
+++ practice/sport-clock/script/soprt_clock.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yurong on 2016/4/19.
+ */
+$(function(){
+    var height = $(window).height();
+
+    $('.sport-main').css("min-height",height + 'px');
+    //$('.add-main').css("min-height",height + 'px');
+})
