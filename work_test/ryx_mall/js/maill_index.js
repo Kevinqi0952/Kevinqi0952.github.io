115493c3414c5bbe8ef5c2742404155fd91fc90a
BigMouth <372219097@qq.com>
2016-04-29 17:49:45 +0800

    change

--- work_test/ryx_mall/js/maill_index.js
+++ work_test/ryx_mall/js/maill_index.js
@@ -1,21 +1,48 @@
 /**
  * Created by yurong on 2016/4/29.
  */
-$(function(){
-    //导航栏顶部吸附
-    var navTop = $('.mall-nav-fixed').offset().top;
+//$(function(){
+//    //导航栏顶部吸附
+//    var navTop = $('.mall-nav-fixed').offset().top;
+//    var fixeHigh = $('.mall-nav-fixed').height();
+//
+//    $(window).scroll(function(){
+//        console.log(fixeHigh);
+//        if($(window).scrollTop() >= navTop){
+//            $('.mall-nav-fixed').css({
+//                "position":"fixed",
+//                "top":"0"
+//            });
+//            $(".content-hot").css("margin-top",fixeHigh + "px")
+//        } else{
+//            $('.mall-nav-fixed').css({
+//                "position":"relative"
+//            });
+//            $(".content-hot").css("margin-top","0")
+//        }
+//    })
+//})
 
-    $(window).scroll(function(){
-        console.log(fixeHigh);
-        if($(window).scrollTop() >= navTop){
-            $('.mall-nav-fixed').css({
-                "position":"fixed",
-                "top":"0"
-            });
-        } else{
-            $('.mall-nav-fixed').css({
-                "position":"relative"
-            });
-        }
-    })
-})
+function menuFixed(id){
+    var obj = document.getElementById("mall-nav-fixed");
+    var _getHeight = obj.offsetTop;
+
+    window.onscroll = function(){
+        changePos(id,_getHeight);
+    }
+}
+function changePos(id,height){
+    var obj = document.getElementById("mall-nav-fixed");
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scrollTop < height){
+        obj.style.position = 'relative';
+    }else{
+        obj.style.position = 'fixed';
+        obj.style.top = "0";
+        /*给元素添加`fixed`固定属性
+         也可以添加写好的CSS样式`obj.className = 'fix_css';`*/
+    }
+}
+window.onload = function(){
+        menuFixed('nav');
+    }
