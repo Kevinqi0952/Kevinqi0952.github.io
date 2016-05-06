921adb556a02fbe608d3909633c4ad8a8f80a644
BigMouth <372219097@qq.com>
2016-05-06 16:01:45 +0800

    change

--- work_test/currency_mall/js/mall_vouchers.js
+++ work_test/currency_mall/js/mall_vouchers.js
@@ -1,3 +1,18 @@
 /**
  * Created by yurong on 2016/5/5.
  */
+
+$(function(){
+    $('.vouchers-nav li').click(function(){
+        var aThis = $(this).children("a");
+        var liIndex = $(this).index();
+
+
+        if(!aThis.hasClass('vouchers-active')){
+            $('.vouchers-nav li').children("a").removeClass("vouchers-active");
+            aThis.addClass("vouchers-active");
+            $('.vouchers-details ul').hide();
+            $('.vouchers-details ul').eq(liIndex).fadeIn(200);
+        }
+    })
+})
