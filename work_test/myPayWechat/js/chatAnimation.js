4644f5cb1e8e5698a0fd7e0068f978014aec1792
BigMouth <372219097@qq.com>
2016-06-08 20:48:30 +0800

    change

--- work_test/myPayWechat/js/chatAnimation.js
+++ work_test/myPayWechat/js/chatAnimation.js
@@ -3,16 +3,15 @@
  */
 
 window.onload = $(function(){
-    
     var audioEle = $("#audio")[0];
+    //$('.chat-details').animate({marginTop:'-1rem'})
 
     setTimeout(fadeIn01,1000);
-
     function fadeIn01(){
         $('#chat-info01').css('display','block');
         $('#chat-man01').css('display','block');
         audioEle.play();
-        setTimeout(fadeIn02,1500)
+        setTimeout(fadeIn02,1500);
     }
     function fadeIn02(){
         $('#chat-woman01').css('display','block');
@@ -36,6 +35,41 @@ window.onload = $(function(){
     }
     function fadeIn06(){
         $('#chat-man02').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn07,1500)
+    }
+    function fadeIn07(){
+        $('#chat-man03').css('display','block');;
+        audioEle.play();
+        setTimeout(fadeIn08,1500)
+    }
+    function fadeIn08(){
+        $('#chat-man04').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn09,1500)
+    }
+    function fadeIn09(){
+        $('#chat-woman04').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn10,1500)
+    }
+    function fadeIn10(){
+        $('#chat-man05').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn11,1500)
+    }
+    function fadeIn11(){
+        $('#chat-man06').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn12,1500)
+    }
+    function fadeIn12(){
+        $('#chat-man07').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn13,1500)
+    }
+    function fadeIn13(){
+        $('#chat-woman05').css('display','block');
         audioEle.play();
     }
 })
