3f5d65287e610b4b5e358e5e3fcb26c934765d84
BigMouth <372219097@qq.com>
2016-06-08 12:59:29 +0800

    change

---
+++ work_test/myPayWechat/js/chatAnimation.js
@@ -0,0 +1,39 @@
+/**
+ * Created by yurong on 2016/6/8.
+ */
+$(function(){
+    var audioEle = $("#audio")[0];
+
+    setTimeout(fadeIn01,1000);
+
+    function fadeIn01(){
+        $('#chat-info01').css('display','block');
+        $('#chat-man01').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn02,1500)
+    }
+    function fadeIn02(){
+        $('#chat-woman01').css('display','block');
+        audioEle.play();
+        setTimeout(fadeOut03,1000)
+    }
+    function fadeOut03(){
+        $('#chat-man01').css('display','none');
+        $('#chat-info02').fadeIn();
+        setTimeout(fadeIn04,1500)
+    }
+    function fadeIn04(){
+        $('#chat-woman02').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn05,1500)
+    }
+    function fadeIn05(){
+        $('#chat-woman03').css('display','block');
+        audioEle.play();
+        setTimeout(fadeIn06,1500)
+    }
+    function fadeIn06(){
+        $('#chat-man02').css('display','block');
+        audioEle.play();
+    }
+})
