72d6e8a73362489e1a95658f539c728f1e7d6bc4
吴佳奇 <wujiaqi@wujiaqideMacBook-Pro.local>
2017-02-28 00:42:11 +0800

    add task1 README and change code

--- 2017-baidu-web/ECharts/task1/src/App.vue
+++ 2017-baidu-web/ECharts/task1/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <histogram class="histogram"></histogram>
-    <line-chart></line-chart>
+    <chart></chart>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import histogram from "./components/histogram.vue";
-  import linechart from "./components/linechart.vue";
+  import chart from "./components/chart.vue";
 
   export default{
     components:{
-      'histogram':histogram,
-      'line-chart':linechart
+      'chart':chart
     }
   };
 </script>
