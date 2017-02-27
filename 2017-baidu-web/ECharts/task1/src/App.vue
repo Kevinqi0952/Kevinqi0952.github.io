dcb949d7f19529e47c0fb342f55e69dc71e3dec0
BigMouth <372219097@qq.com>
2017-02-27 19:00:03 +0800

    learn ECharts part 1

---
+++ 2017-baidu-web/ECharts/task1/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <histogram class="histogram"></histogram>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import histogram from "./components/histogram.vue";
+  import linechart from "./components/linechart.vue";
+
+  export default{
+    components:{
+      'histogram':histogram,
+      'line-chart':linechart
+    }
+  };
+</script>
+
+<style>
+
+</style>
