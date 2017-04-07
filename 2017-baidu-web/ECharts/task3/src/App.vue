58744019435e41fd7faf9d1ab3564d290bb01ddf
BigMouth <372219097@qq.com>
2017-04-07 09:48:40 +0800

    add

---
+++ 2017-baidu-web/ECharts/task3/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <chart></chart>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import chart from "./components/chart.vue";
+
+  export default{
+    components:{
+      'chart':chart
+    }
+  };
+</script>
+
+<style>
+
+</style>
---
+++ 2017-baidu-web/ECharts/task3/src/components/chart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="chart">{{ name }}</div>
+</template>
+
+<script type="text/ecmascript-6">
+  const echarts = require('echarts/lib/echarts');
+
+  export default{
+    data(){
+      return {
+        name: 'chart'
+
+      }
+    },
+    methods: {},
+    mounted(){
+      function foo(str,a){
+        eval( str ); //欺骗！
+        console.log(a,b);
+      }
+      var b = 2;
+      foo("var b = 3;",1);//
+    }
+    }
+</script>
+
+<style>
+  #chart {
+    width: 1000px;
+    height: 500px;
+    margin: 20px auto 100px auto;
+  }
+</style>
